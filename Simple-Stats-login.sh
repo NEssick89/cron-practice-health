0abe3d4665e5694a3ef4e0187f5da86d7bf85358
Nathan Essick <nmessick89@gmail.com>
2026-06-10 15:59:48 -0400

    Updated comments and formatting

--- Simple-Stats-login.sh
+++ Simple-Stats-login.sh
@@ -1,12 +1,14 @@
 # Displays system status information at login
 
 # Displays system health from simple_stats.log
-echo ' ' >> ~/.bashrc
+echo >> ~/.bashrc
 echo '# Display health check log at login' >> ~/.bashrc
 echo 'cat /var/log/simple_stats.log' >> ~/.bashrc
 
 # Displays logged in users
 echo >> ~/.bashrc
+echo '# Display active users' >> ~/.bashrc
+echo 'echo' >> ~/.bashrc
 echo 'echo Logged in users:' >> ~/.bashrc
 echo 'users' >> ~/.bashrc
 
