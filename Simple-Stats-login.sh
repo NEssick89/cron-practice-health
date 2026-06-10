1b3ec920c3ce32b82e0a4e8c25848594d45976dd
Nathan Essick <nmessick89@gmail.com>
2026-06-10 15:29:21 -0400

    Blank space added for improved readability

--- Simple-Stats-login.sh
+++ Simple-Stats-login.sh
@@ -1,5 +1,6 @@
 #Displays cron-practice-health log at login
 
+echo ' ' >> ~/.bashrc
 echo '# Display health check log at login' >> ~/.bashrc
 echo 'cat /var/log/simple_stats.log' >> ~/.bashrc
 rm -- "$0"
