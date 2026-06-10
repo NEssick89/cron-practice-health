806d3a0521df3443b738e71f17846b1faecacd58
Nathan Essick <nmessick89@gmail.com>
2026-06-10 15:54:49 -0400

    Updated to include a current user check as well as formatting for clarity

--- Simple-Stats-login.sh
+++ Simple-Stats-login.sh
@@ -1,6 +1,14 @@
-#Displays cron-practice-health log at login
+# Displays system status information at login
 
+# Displays system health from simple_stats.log
 echo ' ' >> ~/.bashrc
 echo '# Display health check log at login' >> ~/.bashrc
 echo 'cat /var/log/simple_stats.log' >> ~/.bashrc
+
+# Displays logged in users
+echo >> ~/.bashrc
+echo 'echo Logged in users:' >> ~/.bashrc
+echo 'users' >> ~/.bashrc
+
+# Script self deletes 
 rm -- "$0"
